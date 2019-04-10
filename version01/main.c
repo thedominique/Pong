6e8aeace5f2c6847cde1f7d56be378ca0318044f
Dominique Persson <niquepersson@gmail.com>
2019-04-10 13:28:57 +0200

    Create main.c

---
+++ version01/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "SDL_net.h"
+#include <SDL2/SDL.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "constants.h"
+
+
+int main(int argc, const char * argv[]) {
+    
+    SDL_Init(SDL_INIT_EVERYTHING);
+    
+    IPaddress server_addr;
+    int local_port = 0;
+    bool is_server = false;
+ 
+    
+    return 0;
+}
