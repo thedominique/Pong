acf527b8a2d8a105864b02a0a917575f9a02dc6e
Dominique Persson <niquepersson@gmail.com>
2019-04-10 13:27:53 +0200

    Create constants.h

---
+++ version01/constants.h
@@ -0,0 +1,23 @@
+#ifndef constants_h
+#define constants_h
+
+const float PAD_WIDTH = 0.02;
+const float PAD_HEIGHT = 0.15;
+const float PAD_PAD = 0.02;
+const float PAD_SPEED_PER_SEC = 1;
+
+const float BALL_WIDTH = 0.015;
+const float BALL_HEIGHT = 0.02;
+const float BALL_SPEED_PER_SEC = 0.4;
+
+const float BALL_INIT_X0 = 0.5 - BALL_WIDTH / 2;
+const float BALL_INIT_Y0 = 0.5 - BALL_HEIGHT / 2;
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+const int SERVER_PORT = 5050;
+
+const float NET_TICK_RATE = 20;
+
+#endif /* constants_h */
