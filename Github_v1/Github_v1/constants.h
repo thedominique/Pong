6c7884349a86e2c2a324439e8ced029db3afe37f
MiguelMaytaLavalle <48988098+MiguelMaytaLavalle@users.noreply.github.com>
2019-04-14 23:01:52 +0200

    Update constants.h

--- Github_v1/Github_v1/constants.h
+++ Github_v1/Github_v1/constants.h
@@ -13,9 +13,34 @@
 #define LOCAL_PORT 1234
 
 //const float PAD_INIT_Y0 = 0;
-const long NET_TICK_RATE = 60;
+const float NET_TICK_RATE = 60;
 const float ballSpeed = BALL_SPEED; //OOF
 const float p1_posX0 = 0, p1_posY0 = 0;
 const float p2_posX0 = SCREEN_W, p2_posY0 = 0;
+const float BALL_INIT_X0 = SCREEN_W / 2;
+const float BALL_INIT_Y0 = SCREEN_H / 2;
+typedef struct ballpacket {
+	float yPos;
+	float xPos;
+	int h_direction;
+	int v_direction;
+	int yangle;
+}ball_packet;
 
+typedef struct IPpacket
+{
+	float ball_yPos;
+	float ball_xPos;
+	int h_dir;
+	int v_dir;
+	int yangle;
+	float pad_Y, pad_X; // byt till pad_y, pad_x??
+	int which_player;
+} IP_packet_t;
+
+typedef struct players
+{
+	float yPos; // använd snake för mer consistency 
+	float xPos;
+}Players;
 #endif // !1
