c55390a05f820d4fe0263315336a91251ebb7ddb
MiguelMaytaLavalle <48988098+MiguelMaytaLavalle@users.noreply.github.com>
2019-04-26 17:21:59 +0200

    Update display_text.c
    Added "nextRoundText" function. Displays "Get Ready For The Next Round!" when a player is out.
    💯

--- Bari v4/client/display_text.c
+++ Bari v4/client/display_text.c
@@ -25,7 +25,7 @@ SDL_Texture *update_text(GameState *gamestate, SDL_Renderer *renderer, OldLives 
 		printf("TTF open error");
 	}
 	//reset msg =0;
-	sprintf(oldLives->msg, "%s: %s %d %s %d %s %d", oldLives->lives, oldLives->purple , gamestate->players[0].lives, oldLives->red, gamestate->players[1].lives, oldLives->yellow, gamestate->players[2].lives);
+	sprintf(oldLives->msg, "%s: %s %d %s %d %s %d", oldLives->lives, oldLives->purple, gamestate->players[0].lives, oldLives->red, gamestate->players[1].lives, oldLives->yellow, gamestate->players[2].lives);
 
 
 	SDL_Color color = { 255,255,255,255 };
@@ -63,9 +63,27 @@ SDL_Texture* init_text(GameState *gamestate, SDL_Renderer *renderer)
 	char str5[6] = "lives";
 	sprintf(str, "%s: %s %d %s %d %s %d", str5, str2, gamestate->players[0].lives, str3, gamestate->players[1].lives, str4, gamestate->players[2].lives);
 
+	SDL_Color color = { 255,255,255,255 };
+	SDL_Surface *textSurface = TTF_RenderText_Solid(font, str, color);
+	SDL_Texture *text = SDL_CreateTextureFromSurface(renderer, textSurface);
 
+	SDL_FreeSurface(textSurface);
 
+	return text;
+}
 
+SDL_Texture* nextRoundText(SDL_Renderer *renderer) {
+	printf("Text\n");
+	TTF_Font *font = TTF_OpenFont("Arial.ttf", 34);
+	if (!font)
+	{
+		printf("TTF open error");
+	}
+	
+	char str[32];
+	char roundStr[32] = "Get Ready For The Next Round!";
+
+	sprintf(str, "%s", roundStr);
 	SDL_Color color = { 255,255,255,255 };
 	SDL_Surface *textSurface = TTF_RenderText_Solid(font, str, color);
 	SDL_Texture *text = SDL_CreateTextureFromSurface(renderer, textSurface);
