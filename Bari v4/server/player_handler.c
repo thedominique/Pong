b995daaf39c07c7f8e58c0b541481896ce9c27ad
barihisham <47780794+barihisham@users.noreply.github.com>
2019-04-28 17:14:37 +0200

    FLAGGORNA TAS EMOT ISTÄLLET FÖR POSITIONER
    FLAGGORNA TAS EMOT ISTÄLLET FÖR POSITIONER

--- Bari v4/server/player_handler.c
+++ Bari v4/server/player_handler.c
@@ -2,19 +2,23 @@
 
 void identify_player(UDPpacket *packet_receive, IPaddress client_ipaddress[], GameState *gamestate)
 {
-    int i = 0;
-    
-    for (i = 0; i < 4; i++)
-    {
-        if (packet_receive->address.host == client_ipaddress[i].host
-            && packet_receive->address.port == client_ipaddress[i].port)
-        {
-            Paddle* receive = (Paddle*)packet_receive->data;
-            gamestate->players[i].x = receive->x;
-            gamestate->players[i].y = receive->y;
-            printf("Player %d made a move", i);
-            //printf("RE: %lf \n", receive->x);
-            
-        }
-    }
+	int i = 0;
+
+	for (i = 0; i < 3; i++)
+	{
+		if (packet_receive->address.host == client_ipaddress[i].host
+			&& packet_receive->address.port == client_ipaddress[i].port)
+		{
+			Paddle* receive = (Paddle*)packet_receive->data;
+
+			gamestate->keys[i].d = receive->k.d;
+			gamestate->keys[i].a = receive->k.a;
+			gamestate->keys[i].w = receive->k.w;
+			gamestate->keys[i].s = receive->k.s;
+			gamestate->keys[i].up = receive->k.up;
+			gamestate->keys[i].down = receive->k.down;
+			//printf("Player %d made a move", i);
+
+		}
+	}
 }
