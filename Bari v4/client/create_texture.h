b4b7c3496a67b0525f1dc0394e14e4317e5ac551
barihisham <47780794+barihisham@users.noreply.github.com>
2019-04-28 18:00:45 +0200

    create texture.h

---
+++ Bari v4/client/create_texture.h
@@ -0,0 +1,12 @@
+#ifndef create_texture_h
+#define create_texture_h
+#include "objects.h"
+
+
+SDL_Texture *init_board_texture(SDL_Renderer *renderer);
+SDL_Texture *init_player1_texture(SDL_Renderer *renderer);
+SDL_Texture *init_player2_texture(SDL_Renderer *renderer);
+SDL_Texture *init_player3_texture(SDL_Renderer *renderer);
+
+
+#endif // !create_texture_h
