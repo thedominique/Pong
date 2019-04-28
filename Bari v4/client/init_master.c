ade345af10d06e839bb93b1f8cdcb3b0c5c483aa
barihisham <47780794+barihisham@users.noreply.github.com>
2019-04-28 17:00:36 +0200

    Update init_master.c

--- Bari v4/client/init_master.c
+++ Bari v4/client/init_master.c
@@ -1,7 +1,7 @@
 #include "init_master.h"
 #include "objects.h"
 
-void initPlayers(GameState* gamestate, OldLives* oldlives) {
+void initPlayers(GameState* gamestate, OldLives* oldlives, Paddle *mypaddle) {
 	printf("Check");
 	gamestate->players[0].x = WINDOW_WIDTH / 2;
 	gamestate->players[0].y = 30;
@@ -11,9 +11,9 @@ void initPlayers(GameState* gamestate, OldLives* oldlives) {
 	gamestate->players[0].greenShade = 0;
 	gamestate->players[0].blueShade = 130;
 	gamestate->players[0].lives = 3;
-	gamestate->players[0].oldPlayer = SDL_FALSE;
 
-	gamestate->players[1].x = WINDOW_WIDTH - -PADDLE_WIDTH;
+
+	gamestate->players[1].x = WINDOW_WIDTH - PADDLE_WIDTH;
 	gamestate->players[1].y = WINDOW_HEIGHT / 2;
 	gamestate->players[1].w = PADDLE_WIDTH;
 	gamestate->players[1].h = PADDLE_HEIGHT;
@@ -21,7 +21,6 @@ void initPlayers(GameState* gamestate, OldLives* oldlives) {
 	gamestate->players[1].greenShade = 0;
 	gamestate->players[1].blueShade = 0;
 	gamestate->players[1].lives = 3;
-	gamestate->players[1].oldPlayer = SDL_FALSE;
 
 	gamestate->players[2].x = 0;
 	gamestate->players[2].y = 0;
@@ -31,7 +30,7 @@ void initPlayers(GameState* gamestate, OldLives* oldlives) {
 	gamestate->players[2].greenShade = 255;
 	gamestate->players[2].blueShade = 11;
 	gamestate->players[2].lives = 3;
-	gamestate->players[2].oldPlayer = SDL_FALSE;
+
 
 	gamestate->ball.x = WINDOW_WIDTH / 2;
 	gamestate->ball.y = WINDOW_HEIGHT / 2;
@@ -50,4 +49,15 @@ void initPlayers(GameState* gamestate, OldLives* oldlives) {
 	strcpy(oldlives->red, "red");
 	strcpy(oldlives->yellow, "yellow");
 	strcpy(oldlives->lives, "Lives");
+
+	mypaddle->x = 30;
+	mypaddle->y = 30;
+	mypaddle->h = PADDLE_HEIGHT;
+	mypaddle->w = PADDLE_WIDTH;
+	mypaddle->k.w = 0;
+	mypaddle->k.w = 0;
+	mypaddle->k.w = 0;
+	mypaddle->k.w = 0;
+	mypaddle->k.up = 0;
+	mypaddle->k.down = 0;
 }
