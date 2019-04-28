ee1fbb46eadec5460a56783f2769211f49b6731c
MiguelMaytaLavalle <48988098+MiguelMaytaLavalle@users.noreply.github.com>
2019-04-28 14:33:35 +0200

    Update objects.h
    La till "oldplayer" boolean för att indikera när x spelare är redan ute ur spelet.

--- Bari v4/client/objects.h
+++ Bari v4/client/objects.h
@@ -1,4 +1,4 @@
-#pragma once
+
 #ifndef objects_h
 #define objects_h
 #include <stdio.h>
@@ -7,11 +7,10 @@
 #include "SDL_net.h"
 #include "SDL.h"
 
-
 static const double BALL_SIZE = 20;
 static const double WINDOW_WIDTH = 1280;
 static const double WINDOW_HEIGHT = 720;
-static const double BALL_SPEED = 0.006;
+static const double BALL_SPEED = 0.0002;
 static const double PADDLE_WIDTH = 20;
 static const double PADDLE_HEIGHT = 100;
 static const double PADDLE_SPEED = 3;
@@ -38,17 +37,14 @@ typedef struct
 	double x, y, h, w;
 	int redShade, greenShade, blueShade;
 	int lives;
+	SDL_bool oldPlayer;
 } Paddle;
-
-
 
 typedef struct
 {
 	double x, y, h, w, xVel, yVel;
 	int redShade, greenShade, blueShade;
 } Ball;
-
-
 
 typedef struct
 {
