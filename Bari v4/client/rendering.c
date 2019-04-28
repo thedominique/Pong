4e3ee838f6f67c5fb16bdaf16def51d4c1559d80
barihisham <47780794+barihisham@users.noreply.github.com>
2019-04-28 17:05:12 +0200

    LAGT TILL IMAGES TILL PADDLES(TRÄTEMA)

--- Bari v4/client/rendering.c
+++ Bari v4/client/rendering.c
@@ -1,6 +1,6 @@
 #include "rendering.h"
 
-void doRender(SDL_Renderer *renderer, GameState *gamestate, Paddle *mypaddle, SDL_Texture *board, SDL_Texture *text) {
+void doRender(SDL_Renderer *renderer, GameState *gamestate, Paddle *mypaddle, SDL_Texture *board, SDL_Texture *text, SDL_Texture *player1Texture, SDL_Texture *player2Texture, SDL_Texture *player3Texture) {
 	int i = 0;
 	//set the drawing color to blue
 	SDL_SetRenderDrawColor(renderer, 0, 0, 30, 255);
@@ -16,14 +16,16 @@ void doRender(SDL_Renderer *renderer, GameState *gamestate, Paddle *mypaddle, SD
 	SDL_Rect boardRect = { WINDOW_WIDTH / 2 - WINDOW_WIDTH / 4 ,WINDOW_HEIGHT / 2 - WINDOW_HEIGHT / 4,WINDOW_WIDTH / 2,WINDOW_HEIGHT / 2 };
 	SDL_RenderCopy(renderer, board, NULL, &boardRect);
 
-	for (i = 0; i < 4; i++) {
-		//SET COLOR
-		SDL_SetRenderDrawColor(renderer, gamestate->players[i].redShade, gamestate->players[i].greenShade, gamestate->players[i].blueShade, 255);
-		//CREATE RECT
-		SDL_Rect player = { gamestate->players[i].x, gamestate->players[i].y, gamestate->players[i].w, gamestate->players[i].h };
-		//SHOW RECT
-		SDL_RenderFillRect(renderer, &player);
-	}
+	SDL_Rect player1 = { gamestate->players[0].x, gamestate->players[0].y, gamestate->players[0].w, gamestate->players[0].h };
+	SDL_RenderCopy(renderer, player1Texture, NULL, &player1);
+	
+	SDL_Rect player2 = { gamestate->players[1].x, gamestate->players[1].y, gamestate->players[1].w, gamestate->players[1].h };
+	SDL_RenderCopy(renderer, player2Texture, NULL, &player2);
+	
+	SDL_Rect player3 = { gamestate->players[2].x, gamestate->players[2].y, gamestate->players[2].w, gamestate->players[2].h };
+	SDL_RenderCopy(renderer, player3Texture, NULL, &player3);
+
+
 	//set the drawing color to white
 	SDL_SetRenderDrawColor(renderer, gamestate->ball.redShade, gamestate->ball.greenShade, gamestate->ball.blueShade, 255);
 	SDL_Rect ball = { gamestate->ball.x, gamestate->ball.y, gamestate->ball.w, gamestate->ball.h };
