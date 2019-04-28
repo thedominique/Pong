a3cb7b1817a214f2097923662b0f81e6d8c2325a
barihisham <47780794+barihisham@users.noreply.github.com>
2019-04-28 16:59:18 +0200

    COLLSION SKER ENDAST PÅ SERVER, FLAGGOR SKICKAS
    -COLLISION BORTTAGEN FRÅN CLIENT
    -FLAGGOR SKICKAS FRÅN SERVER

--- Bari v4/client/event_handler.c
+++ Bari v4/client/event_handler.c
@@ -1,32 +1,10 @@
 #include "event_handler.h"
 
-int collision_check(Paddle *mypaddle)
-{
-	if (WINDOW_HEIGHT / 2 - WINDOW_HEIGHT / 4 >= mypaddle->y + mypaddle->h)
-	{
-		return 0;
-	}
-	if (WINDOW_WIDTH / 2 - WINDOW_WIDTH / 4 >=  mypaddle->x )
-	{
-		return 0;
-	}
-	if (WINDOW_HEIGHT / 2 - WINDOW_HEIGHT / 4 + WINDOW_HEIGHT / 2 <= mypaddle->y)
-	{
-		return 0;
-	}
-	if (WINDOW_WIDTH / 2 - WINDOW_WIDTH / 4 + WINDOW_WIDTH / 2 <= mypaddle->x + mypaddle->w)
-	{
-		return 0;
-	}
-	return 1;
-}
-
 int processEvents(SDL_Window *window, Paddle *mypaddle, double dt)
 {
 	SDL_Event event;
 	int done = 0;
 
-	
 
 	//DETECT KEYS (ESC, CLOSE(X))
 	while (SDL_PollEvent(&event))
@@ -59,49 +37,43 @@ int processEvents(SDL_Window *window, Paddle *mypaddle, double dt)
 			break;
 		}
 	}
-
-
-
-	// SKICKA FLAGGOR SOM SÄGER GÅ FRAMÅT, OCH SEDAN RESET DOM OM DET HÄR SKA VARA I SERVER
-
+	//FLAGGOR TILL SERVER, COLLISION HANTERAS AV SERVERN
 	const Uint8 *state = SDL_GetKeyboardState(NULL);
-	if (state[SDL_SCANCODE_W] && mypaddle->y >= 0)
+	if (state[SDL_SCANCODE_W])
 	{
-		mypaddle->y -= PADDLE_SPEED;
-
-		if (collision_check(mypaddle))
-		{
-			mypaddle->y += PADDLE_SPEED;
-		}
+		mypaddle->k.w = 1;
 	}
-	if (state[SDL_SCANCODE_S] && mypaddle->y < WINDOW_HEIGHT - mypaddle->h && !collision_check(mypaddle))
+	if (state[SDL_SCANCODE_S])
 	{
-		mypaddle->y += PADDLE_SPEED;
-
-		if (collision_check(mypaddle))
-		{
-			mypaddle->y -= PADDLE_SPEED;
-		}
+		mypaddle->k.s = 1;
 	}
-	if (state[SDL_SCANCODE_D] && mypaddle->x < WINDOW_WIDTH - mypaddle->w && !collision_check(mypaddle))
+	if (state[SDL_SCANCODE_D])
 	{
-		mypaddle->x += PADDLE_SPEED;
-
-		if (collision_check(mypaddle))
-		{
-			mypaddle->x -= PADDLE_SPEED;
-		}
+		mypaddle->k.d = 1;
 	}
-	if (state[SDL_SCANCODE_A] && mypaddle->x > 0 && !collision_check(mypaddle))
+	if (state[SDL_SCANCODE_A])
 	{
-		mypaddle->x -= PADDLE_SPEED;
-
-		if (collision_check(mypaddle))
-		{
-			mypaddle->x += PADDLE_SPEED;
-		}
+		mypaddle->k.a = 1;
+	}
+	if (state[SDL_SCANCODE_UP])
+	{
+		mypaddle->k.up = 1;
+	}
+	if (state[SDL_SCANCODE_DOWN])
+	{
+		mypaddle->k.down = 1;
 	}
 
 
 	return done;
+}
+
+void reset_keys(Paddle *mypaddle)
+{
+	mypaddle->k.w = 0;
+	mypaddle->k.s = 0;
+	mypaddle->k.a = 0;
+	mypaddle->k.d = 0;
+	mypaddle->k.up = 0;
+	mypaddle->k.down = 0;
 }
