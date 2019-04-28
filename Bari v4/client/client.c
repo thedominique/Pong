6be137e768a96056d2a6b46a4d2ff8d7dfdd1087
barihisham <47780794+barihisham@users.noreply.github.com>
2019-04-28 16:55:50 +0200

    UPDATE CLIENT.C
    NY UPDATE:
    -COLLISION SKER BARA I SERVER
    -COLLISION MELLAN PADDELS
    -BOLLENS HASTIGHET ÖKAR VARJE GÅNG BOLLEN TRÄFFAR EN PADDLE
    -TRÄ TEMA PÅ PADDLES
    -UP ARROW OCH DOWN ARROW BYTER PADDLES WIDTH OCH HEIGHT ( COLLISION FUNGERAR HÄR OCKSÅ)
    -UPDATERADE FUNKTIONER SOM BYTER BOLLENS FÄRG

--- Bari v4/client/client.c
+++ Bari v4/client/client.c
@@ -10,8 +10,11 @@
 #include "SDL_image.h"
 #include "SDL_ttf.h"
 #include "display_text.h"
+#include "create_texture.h"
+#include "init_master.h"
 
-int main(int argc, char **argv){
+
+int main(int argc, char **argv) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDLNet_Init();
 	UDPsocket client_socket;
@@ -19,63 +22,23 @@ int main(int argc, char **argv){
 	UDPpacket *packet_send;
 	UDPpacket *packet_receive;
 
+	
 	int choice = menu();
-	if (choice == 2){
+	if (choice == 2) {
 		return 0;
 	}
 	
+
 	GameState gamestate;
-
-	gamestate.players[0].x = 0;
-	gamestate.players[0].y = WINDOW_HEIGHT / 2;
-	gamestate.players[0].w = PADDLE_WIDTH;
-	gamestate.players[0].h = PADDLE_HEIGHT;
-	gamestate.players[0].redShade = 75;
-	gamestate.players[0].greenShade = 0;
-	gamestate.players[0].blueShade = 130;
-	gamestate.players[0].lives = 3;
-
-	gamestate.players[1].x = WINDOW_WIDTH - PADDLE_WIDTH;
-	gamestate.players[1].y = WINDOW_HEIGHT / 2;
-	gamestate.players[1].w = PADDLE_WIDTH;
-	gamestate.players[1].h = PADDLE_HEIGHT;
-	gamestate.players[1].redShade = 255;
-	gamestate.players[1].greenShade = 0;
-	gamestate.players[1].blueShade = 0;
-	gamestate.players[1].lives = 3;
-
-	gamestate.players[2].x = 0;
-	gamestate.players[2].y = 0;
-	gamestate.players[2].w = PADDLE_WIDTH;
-	gamestate.players[2].h = PADDLE_HEIGHT;
-	gamestate.players[2].redShade = 248;
-	gamestate.players[2].greenShade = 255;
-	gamestate.players[2].blueShade = 11;
-	gamestate.players[2].lives = 3;
-
-	gamestate.ball.x = WINDOW_WIDTH / 2;
-	gamestate.ball.y = WINDOW_HEIGHT / 2;
-	gamestate.ball.h = BALL_SIZE;
-	gamestate.ball.w = BALL_SIZE;
-	gamestate.ball.xVel = BALL_SPEED;
-	gamestate.ball.yVel = BALL_SPEED;
-	gamestate.ball.redShade = 75;
-	gamestate.ball.greenShade = 0;
-	gamestate.ball.blueShade = 130;
-
+	Paddle mypaddle;
 	OldLives oldLives;
-	oldLives.player1Lives = gamestate.players[0].lives = 3;
-	oldLives.player2Lives = gamestate.players[1].lives = 3;
-	oldLives.player3Lives = gamestate.players[2].lives = 3;
-	strcpy(oldLives.purple, "purple");
-	strcpy(oldLives.red, "red");
-	strcpy(oldLives.yellow, "yellow");
-	strcpy(oldLives.lives, "Lives");
-
-	float NET_TICK_RATE = 30;
+	initPlayers(&gamestate, &oldLives, &mypaddle);
+	
+	
+	float NET_TICK_RATE = 60;
 
 	/* Open a socket on random port */
-	client_socket = SDLNet_UDP_Open(2344);
+	client_socket = SDLNet_UDP_Open(2317);
 
 	/* Resolve server name  */
 	SDLNet_ResolveHost(&ipaddress, "localhost", 1234);
@@ -86,11 +49,6 @@ int main(int argc, char **argv){
 
 	SDL_Window *window;
 	SDL_Renderer *renderer;
-	Paddle mypaddle;
-	mypaddle.x = 30;
-	mypaddle.y = 30;
-	mypaddle.h = PADDLE_HEIGHT;
-	mypaddle.w = PADDLE_WIDTH;
 
 	//Create an application window with the following settings:
 	window = SDL_CreateWindow("Game Window",                     // window title
@@ -102,22 +60,16 @@ int main(int argc, char **argv){
 	);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-	//HANTERAR BILD (BORDET)
+	//HANTERAR BILD (BORDET,SPELARE)
 	IMG_Init(IMG_INIT_JPG);
 
-	//SDL_Texture* backround;
-
-	SDL_Texture *board;
-	SDL_Surface *boardSurface = NULL;
-	boardSurface = IMG_Load("bana.jpg");
-	if (boardSurface == NULL){
-		printf("Could not find image! \n");
-		SDL_Quit();
-		return 1;
-	}
-	board = SDL_CreateTextureFromSurface(renderer, boardSurface);
-	SDL_FreeSurface(boardSurface);
-
+	//BOARD AND PLAYERS TEXTURES
+	SDL_Texture *board = init_board_texture(renderer);
+	SDL_Texture *player1Texture = init_player1_texture(renderer);
+	SDL_Texture *player2Texture = init_player2_texture(renderer);
+	SDL_Texture *player3Texture = init_player3_texture(renderer);
+	
+	//TEXT TEXTURES
 	SDL_Texture *text = init_text(&gamestate, renderer);
 
 	int done = 0;
@@ -127,11 +79,11 @@ int main(int argc, char **argv){
 	long next_net_tick = tick_t0;
 	long net_tick_interval = (1 / NET_TICK_RATE) * ticks_per_sec;
 
-	while (!done){
+	while (!done) {
 		long tick_t1 = SDL_GetPerformanceCounter();
 		double dt = (tick_t1 - tick_t0) / (double)ticks_per_sec;
 
-		if (SDLNet_UDP_Recv(client_socket, packet_receive)){
+		if (SDLNet_UDP_Recv(client_socket, packet_receive)) {
 			Receive *receive = (Receive *)packet_receive->data;
 			//printf("RECEIVED: %lf\n", receive->b1.x);
 			//printf("gamestate player[0].x: %lf\n", gamestate.players[0].x);
@@ -139,35 +91,42 @@ int main(int argc, char **argv){
 		}
 
 		//update texten OM det har skett någon skillnad
-		if (compare_lives(&gamestate, &oldLives)){
+		if (compare_lives(&gamestate, &oldLives)) {
 			printf("YES");
+
 			text = update_text(&gamestate, renderer, &oldLives);
+			
 		}
 
 		//Render display
-		doRender(renderer, &gamestate, &mypaddle, board, text);
-		
-		done = processEvents(window, &mypaddle, dt);
-		if (tick_t1 >= next_net_tick){
-			for (i = 0; i < 4; i++){
+		doRender(renderer, &gamestate, &mypaddle, board, text, player1Texture,player2Texture, player3Texture);
+
+		done = processEvents(window, &mypaddle, dt, &gamestate);
+		if (tick_t1 >= next_net_tick) {
+			for (i = 0; i < 4; i++) {
 				packet_send->data = (void*)&mypaddle;
 				packet_send->channel = -1;
 				packet_send->len = sizeof(mypaddle);
 				packet_send->maxlen = packet_send->len + 20;
 				packet_send->address = ipaddress;
-				
-				if (SDLNet_UDP_Send(client_socket, -1, packet_send) == 0){
+
+				if (SDLNet_UDP_Send(client_socket, -1, packet_send) == 0) {
 					printf("failed to send from client/n");
 				}
 			}
 			next_net_tick += net_tick_interval;
 		}
+		//RESET KEYS TILL NÄSTA LOOP OMGÅNG
+		reset_keys(&mypaddle);
 	}
-	SDL_DestroyTexture(board);
 	SDLNet_FreePacket(packet_send);
 	SDLNet_FreePacket(packet_receive);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyTexture(text);
+	SDL_DestroyTexture(board);
+	SDL_DestroyTexture(player1Texture);
+	SDL_DestroyTexture(player2Texture);
+	SDL_DestroyTexture(player2Texture);
 	SDL_DestroyWindow(window);
 	IMG_Quit();
 
