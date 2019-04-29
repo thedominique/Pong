13b4a4eabf1f42f19cd9591fc9120192d60b4823
abbemannen <48988103+abbemannen@users.noreply.github.com>
2019-04-29 14:33:15 +0200

    soundeffect.h
    h fil för ljudeffekter

---
+++ Main Project/client/soundeffect.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef soundeffect_h
+#define soundeffect_h
+#include <SDL_mixer.h>
+
+//Ljudeffekter hittade http://soundbible.com/1120-Bounce.html //
+
+typedef struct sound{
+	Mix_Music *soundeffect;
+	int channel;
+}sound;
+
+void initsound(sound *soundeffect);
+
+#endif	soundeffect_h
