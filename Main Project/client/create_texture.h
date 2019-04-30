28d73c6a052ef1e952074a7b7e4bcd19f0505da1
Dominique Persson <niquepersson@gmail.com>
2019-04-30 19:35:56 +0200

    Update create_texture.h

--- Main Project/client/create_texture.h
+++ Main Project/client/create_texture.h
@@ -2,11 +2,9 @@
 #define create_texture_h
 #include "objects.h"
 
-
 SDL_Texture *init_board_texture(SDL_Renderer *renderer);
 SDL_Texture *init_player1_texture(SDL_Renderer *renderer);
 SDL_Texture *init_player2_texture(SDL_Renderer *renderer);
 SDL_Texture *init_player3_texture(SDL_Renderer *renderer);
-
 
 #endif // !create_texture_h
