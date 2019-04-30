6012d7ff5b35d8c06c72b02e4d63e700ede93bab
MiguelMaytaLavalle <48988098+MiguelMaytaLavalle@users.noreply.github.com>
2019-04-30 12:46:27 +0200

    Update collision.h
    added "isPlayerOut" function

--- Main Project/server/collision.h
+++ Main Project/server/collision.h
@@ -6,15 +6,27 @@
 #include "objects.h"
 
 void collision_with_paddles(GameState *gamestate, int i);
+
 int collision_check(GameState *gamestate, int i);
+
 void detect_collision(GameState *gamestate);
+
 int negativ_or_positiv(double velocity);
+
 int which_player(GameState *gamestate);
+
 bool equal_color(GameState *gamestate, int i);
+
 void give_next_color(GameState *gamestate, int i);
+
 void resetState(GameState* gamestate);
+
 void kill_player(GameState *gamestate);
+
 void next_color_after_death(GameState *gamestate);
+
 void increase_ball_speed(GameState *gamestate);
+
+void isPlayerOut(GameState* gamestate);
 
 #endif // !collision_h
