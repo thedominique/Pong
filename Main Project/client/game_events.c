eaeac04035959afc14b444b3cc65a97e24b4dbea
MiguelMaytaLavalle <48988098+MiguelMaytaLavalle@users.noreply.github.com>
2019-05-01 23:11:23 +0200

    Create game_events.c Review after previous pull
    Jag vet att nu kommer jag över max 5 limit, men känns som att det borde vara bäst att lägga upp den nu så att man kan testa om tidigare pull request fungerar utan problem.
    
    Should be reviewed after previous pull request are done.
    Added two functions in them.
    "request_Next_Round" just switches runBall boolean to False when a player is out.
    "isPlayerOut" adds players gameOver state to TRUE if the play has no health left.

---
+++ Main Project/client/game_events.c
@@ -0,0 +1,21 @@
+#include "game_events.h"
+#include <SDL.h>
+#include "display_text.h"
+#include "rendering.h"
+
+void request_Next_Round(Paddle* mypaddle) {
+	mypaddle->runBall = SDL_FALSE;
+}
+
+void isPlayerOut(GameState* gamestate, Paddle* mypaddle) {
+	for (int i = 0; i < 3; i++)
+	{
+		if (gamestate->players[i].lives == 0 && gamestate->players[i].gameOver == SDL_FALSE)
+		{
+			gamestate->players[i].gameOver = SDL_TRUE;
+			gamestate->runBall = SDL_FALSE;
+			printf("Player %d is out\n", i);
+			request_Next_Round(mypaddle);
+		}
+	}
+}
